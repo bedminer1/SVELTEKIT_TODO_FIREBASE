f9951fd06f36219643e6819b472750a65ecd345f
bedminer1 <alexander.seah.min.rui@gmail.com>
2023-11-06 19:12:21 +0800

    editing and deleting todo functionality

--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -14,12 +14,36 @@
         todoList = [...todoList, currentTodo]
         currentTodo = ''
     }
+
+    // @ts-ignore
+    // @ts-ignore
+    function editTodo(index) {
+        let newTodoList = todoList.filter((val,i) => {
+            return i !== index
+        })
+
+        currentTodo = todoList[index]
+        todoList = newTodoList
+    }
+
+    // @ts-ignore
+    function removeTodo(index) {
+        let newTodoList = todoList.filter((val,i) => {
+            return i !== index
+        })
+
+        todoList = newTodoList
+    }
 </script>
 
 <div class="mainContainer">
     <div class="headerContainer">
             <h1>Todo List</h1>
-            <button>Save</button>
+            <div class="btn-container">
+                <button>Save</button>
+                <button>Logout</button>
+            </div>
+           
     </div>
     <div class={"enterTodo" + (error ? 'errorBorder' : '')}>
         <form on:submit|preventDefault={addTodo}>
@@ -27,8 +51,21 @@
         </form>
     </div>
     <main>
+        {#if todoList.length === 0}
+            <p style="font-size: 25px;">Nothing To Do</p>
+        {/if}
         {#each todoList as todo, index}
-            <div class='todo'><p>{index + 1}: {todo}</p></div>
+            <div class='todo'><p>{index + 1}: {todo}</p>
+                <div class="actions">
+                    <!-- svelte-ignore a11y-click-events-have-key-events -->
+                    <!-- svelte-ignore a11y-no-noninteractive-element-interactions -->
+                    <img on:click={() => {editTodo(index)}} src="/icons/edit.png" alt="edit">
+                    <!-- svelte-ignore a11y-no-noninteractive-element-interactions -->
+                    <!-- svelte-ignore a11y-click-events-have-key-events -->
+                    <img on:click={() => {removeTodo(index)}} src="/icons/delete.png" alt="delete">
+                </div>
+            </div>
+            
         {/each}
     </main>
 </div>
@@ -47,8 +84,27 @@
     .headerContainer {
         display: flex;
         align-items: center;
-        justify-content: space-between;
+        justify-content: end;
+        gap: 35%;
     }
+
+    h1 {
+        font-size: 50px;
+        text-align: center;
+    }
+
+    button {
+        background-color: rgb(21, 21, 21);
+        width: auto;
+        height: 30px;
+        font-size: 20px;
+        border: rgb(106, 106, 106) 1px solid;
+        border-right: none;
+        border-top: none;
+        border-radius: 2px;
+        padding: 3px;
+    }
+
 
     main {
         display: flex;
@@ -71,17 +127,41 @@
         outline: none;
         border: none;
         padding: 14px;
-        color: #decff1;
+        font-size: 30px;
         border: grey dotted 1px;
     }
 
     .todo {
         display: flex;
         align-items: center;
+        justify-content: space-between;
         color: #decff1;
         background-color: rgb(24, 24, 24);
-        width: 200px;
+        width: 400px;
         height: 30px;
         padding: 12px;
+        border-bottom: grey 1px solid;
+        border-left: rgb(155, 78, 11) 4px solid;
+    }
+
+    .actions {
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        width: 60px;
+        gap: 8px;
+        font-size: 1.3rem;
+    }
+
+    .actions img {
+        width: 25px;
+        color: white;
+        cursor:pointer;
+        -webkit-filter: grayscale(1) invert(1);
+        filter: grayscale(1) invert(1);
+    }
+
+    .actions img:hover {
+        border: 1px solid coral;
     }
 </style>
